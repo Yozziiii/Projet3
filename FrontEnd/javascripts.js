49fe466e9bd25e584b79e2db1ed860c506a1f102
Yozziiii <bryandsg@hotmail.fr>
2025-06-20 17:42:05 +0200

    bouton trier debut
    bontou "tous" fonctionne que si on appuie sur le bouton objet avant

--- FrontEnd/javascripts.js
+++ FrontEnd/javascripts.js
@@ -20,28 +20,20 @@ function gTravaux(projet) {
 
     for (let pj of projet) {
         const figure = document.createElement("figure")
-        console.log(pj)
+      
         const title = document.createElement("p")
         title.innerText = pj.title
 
         const img = document.createElement("img")
         img.src = pj.imageUrl
         img.alt = pj.title
-        console.log(img.src)
-        /*categoryId = pj.category.id et 
-        userId = 1 pour tout les travaux 
-        existant de bases donc les 11 premiers */
-        const categoryId = pj.categoryId
-        const userId = pj.userId
-
-        const category = pj.category
-        const id = pj.id
 
         gallery.appendChild(figure);
         figure.appendChild(img)
         figure.appendChild(title)
 
     }
+    
 }
 
 gTravaux(projet)
@@ -58,24 +50,39 @@ function gfilterButton(categoryR) {
 
     const filterAll = document.createElement("button")
     filterAll.innerText = "Tous"
+    filterAll.classList.add("btn-filter")
+
     navElement.appendChild(filterAll)
 
-    const filterBtn = []
+    filterAll.addEventListener("click", function () {
+        console.log("suisla")
+         gTravaux(projet)
+    })
+
+
 
     for (let cat of categoryR) {
 
         const filterButton = document.createElement("button")
         filterButton.innerText = cat.name
         filterButton.classList.add("btn-filter")
-        filterButton.addEventListener("click", function () {
-            console.log(cat)
-        })
         navElement.appendChild(filterButton)
+        
+        filterButton.addEventListener("click", function () {
+            console.log(filterButton)
+            const projetFiltrees = projet.filter(function (pj) {
+                return pj.category.name === filterButton.innerText
+            })
+            document.querySelector(".gallery").innerHTML = "";
+            gTravaux(projetFiltrees)
+            console.log(projetFiltrees)
+        }
+    )
+
+
     }
 
-    filterAll.addEventListener("click", function () {
-        return projet
-    })
+
 }
 
 
